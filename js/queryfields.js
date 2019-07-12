fdd335ab10a2c53276495ae0a0001410ca90e58a
NetDenizen <NetDenizen@users.noreply.github.com>
2019-07-12 06:28:33 -0400

    Fix date range search bug

--- js/queryfields.js
+++ js/queryfields.js
@@ -111,8 +111,8 @@ function newDateSearcher(minName, maxName, lookup, manager) {
 
 	output._ParseDate = function() {
 		//TODO: Formatting
-		var minDateTime = this.targetMinElement.valueAsDate === null ? new Date(1970, 0, 1, 0, 0, 0).getTime() : this.targetMinElement.valueAsDate.getTime();
-		var maxDateTime = this.targetMaxElement.valueAsDate === null ? new Date(2038, 0, 19, 3, 14, 7).getTime() : this.targetMaxElement.valueAsDate.getTime();
+		var minDateTime = ( this.targetMinElement.valueAsDate === null ? new Date(1970, 0, 1, 0, 0, 0).getTime() : this.targetMinElement.valueAsDate.getTime() ) / 1000;
+		var maxDateTime = ( this.targetMaxElement.valueAsDate === null ? new Date(2038, 0, 19, 3, 14, 7).getTime() : this.targetMaxElement.valueAsDate.getTime() ) / 1000;
 		this.results = newIdRecord([], []);
 		if(minDateTime <= maxDateTime) {
 			this.results.extend( this.lookup.GetNumericalRange(minDateTime, maxDateTime) )
