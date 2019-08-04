99ec50ec1def0c093c1cf69b59ae6454b48f1935
NetDenizen <NetDenizen@users.noreply.github.com>
2019-08-03 20:53:32 -0400

    Remove all logging.js related components.

--- js/main.js
+++ js/main.js
@@ -6,17 +6,14 @@ var config = {
 
 var dbId = "DbFile";
 
-var logger = undefined;
 var db = undefined;
 var searcher = undefined;
 var documentUi = undefined;
 
 function LoadSearcher() {
-	searcher = newStorySearcher(logger, db);
-	logger.LogInfo("Building UI.");
+	searcher = newStorySearcher(db);
 	documentUi = newUiManager(searcher, 'documentUi', defaultListClasses, defaultPageNumber, defaultResultsPerPage);
 	OnBodyResize();
-	logger.LogInfo("Init completed.");
 }
 
 function OpenDb(path) {
@@ -31,15 +28,11 @@ function OpenDb(path) {
 
 function OnSQLSelect() {
 	var path = document.getElementById(dbId).files[0];
-	logger.LogInfo("Loading DB file at path: '" + path.name + "'");
 	OpenDb(path);
 }
 
 function OnPageLoad() {
-	logger = newLogManager(errorId, undefined);
-	logger.LogInfo("Log started.");
 	initSqlJs(config);
-	logger.LogInfo("SQL.js initialized.");
 	OnBodyResize();
 }
 
