c83416199fe3339d53da0c3eb826a81eebdfd71a
NetDenizen <NetDenizen@users.noreply.github.com>
2019-08-03 21:48:16 -0400

    Add loading indicator.

--- js/main.js
+++ js/main.js
@@ -12,11 +12,16 @@ var searcher = undefined;
 var documentUi = undefined;
 
 function LoadSearcher() {
+	var startTime = new Date();
+	SetHTMLToText( document.getElementById("DbLoadStatus"), "Opening SQLite file" );
 	searcher = newStorySearcher(logger, db);
 	logger.LogInfo("Building UI.");
 	documentUi = newUiManager(searcher, 'documentUi', defaultListClasses, defaultPageNumber, defaultResultsPerPage);
 	OnBodyResize();
 	logger.LogInfo("Init completed.");
+	SetHTMLToText( document.getElementById("DbLoadStatus"),
+				   "SQLite file loaded in " + ( ( new Date() - startTime ) / 1000.0 ).toString()  + " seconds."
+				 );
 }
 
 function OpenDb(path) {
@@ -41,6 +46,7 @@ function OnPageLoad() {
 	initSqlJs(config);
 	logger.LogInfo("SQL.js initialized.");
 	OnBodyResize();
+	SetHTMLToText( document.getElementById("DbLoadStatus"), "Waiting for SQLite file" );
 }
 
 document.addEventListener("DOMContentLoaded", OnPageLoad);
